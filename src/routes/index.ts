0991fc0e21772c1234a13064402a765421105012
jianghong <1637124239@qq.com>
2025-01-04 10:56:16 +0800

    根路径

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,5 @@ import {createRouter, createWebHistory} from 'vue-router'
 import { routes } from './config'
 export const router = createRouter({
     routes,
-    history: createWebHistory()
+    history: createWebHistory('/script-dog-doc/')
 })
