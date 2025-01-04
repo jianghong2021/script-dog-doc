bdd350b5429bfc76388a9b19a72011dfd7e74be1
jianghong <1637124239@qq.com>
2025-01-04 11:33:50 +0800

    辅助授权

--- src/pages/BackupAuth.vue
+++ src/pages/BackupAuth.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-const initParams = () => {
-    const params = location.href.split('#')[1]
-    dog.backupAuth(params)
-}
-onMounted(() => {
-    initParams()
-})
+
 </script>
 <template>
     <div class="w-full h-screen flex justify-center items-center">
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const init = ()=>{
+    const params = location.href.split('#')[1]
+    if(params){
+        dog.backupAuth(params)
+    }
+}
+onMounted(() => {
+    init()
+})
+</script>
 <template>
     <div class="w-full h-screen flex justify-center items-center">
         <h3 class="text-2xl font-bold">
