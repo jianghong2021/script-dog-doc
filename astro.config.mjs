d0c453db5a0a2b0736b35b13da0dad47935caca3
jianghong <jianghong2020@qq.com>
2025-01-07 13:50:30 +0800

    完成一版

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,8 @@ export default defineConfig({
 			favicon: '/logo.png',
 			defaultLocale: 'root',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/jianghong2021',
+				'x.com': 'https://x.com/hong_jiang70969',
 			},
 			locales: {
 				root: {
@@ -33,27 +34,60 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					
-					translations:{
+					translations: {
 						'zh-CN': '指南',
 						'en': 'Guides',
 						'zh-TW': '指南',
 					},
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ 
+						{
 							label: '快速开始',
-							 slug: 'guides/start'
-						 },
-						{ 
-							label: 'Example Guide',
-							 slug: 'guides/example'
-						 },
+							slug: 'guides/start',
+							translations:{
+								'zh-CN': '快速开始',
+								'en': 'Quick Start',
+								'zh-TW': '快速開始',
+							},
+						},
+						{
+							label: 'Examples',
+							slug: 'guides/example',
+							translations:{
+								'zh-CN': '示例',
+								'en': 'Examples',
+								'zh-TW': '示例',
+							},
+						},
 					],
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					translations: {
+						'zh-CN': '参考',
+						'en': 'Reference',
+						'zh-TW': '參考',
+					},
+					items:[
+						{
+							label: 'Template',
+							slug: 'reference/template',
+							translations:{
+								'zh-CN': '模板',
+								'en': 'Template',
+								'zh-TW': '模板',
+							},
+						},
+						{
+							label: 'Code',
+							slug: 'reference/code',
+							translations:{
+								'zh-CN': '编程',
+								'en': 'Code',
+								'zh-TW': '程式',
+							},
+						},
+					]
 				},
 			],
 		}),
